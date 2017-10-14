7da2a5541ed62bcb3265331979ea5e5bda6e041c
sdkuker <sdkuker@gmail.com>
2017-10-14 13:55:45 -0500

    Before output management

--- Webpack/src/index.js
+++ Webpack/src/index.js
@@ -1,19 +1,10 @@
 import _ from 'lodash';
-import './style.css';
-import CruisePicture from './cruisePicture.jpg';
 
 function component() {
     var element = document.createElement('div');
 
     // Lodash now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-
-    // add the picture to the existing div
-    var myCruisePicture = new Image();
-    myCruisePicture.src = CruisePicture;
-
-    element.appendChild(myCruisePicture);
 
     return element;
 }
