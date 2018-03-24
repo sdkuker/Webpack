37b0253acd0968a0dce6ec344632aea384b7d214
sdkuker <sdkuker@gmail.com>
2018-03-24 15:13:07 -0500

    can draw a basic svg element like a circle and some text

--- my-app-typescript/src/components/map.tsx
+++ my-app-typescript/src/components/map.tsx
@@ -1,35 +1,16 @@
 import * as React from 'react';
-import './Hello.css';
-import { observer } from 'mobx-react';
-import * as DomainStore from '../types/stores/domainStore';
 
-@observer
 class Map extends React.Component {
 
   render() {
     return (
-      <div className="hello">
-        <div className="greeting">
-          Hello {DomainStore.domainState.name + getExclamationMarks(DomainStore.domainState.enthusiasmLevel)}
-        </div>
-        <div>
-          <button onClick={DomainStore.domainState.incrementEnthusiasmLevel}>+</button>
-          <button onClick={DomainStore.domainState.decrementEnthusiasmLevel}>-</button>
-        </div>
-      </div>
+      <svg width={window.innerWidth} height={window.innerHeight} >
+        <circle cx="50" cy="50" r="40" stroke="green" stroke-width="4" fill="yellow" />
+        <text x="0" y="115" fill="red">Steveie's svg text</text>
+      </svg>
     );
   }
 
 }
 
 export default Map;
-
-// helpers
-
-function getExclamationMarks(numChars: number) {
-  if (numChars < 1) {
-    return Array(1).join('!');
-  } else {
-      return Array(numChars + 1).join('!');
-  }
-}
