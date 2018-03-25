229a3e295c7946df6bc8e36b913dc6b80df38301
sdkuker <sdkuker@gmail.com>
2018-03-25 15:14:53 -0500

    Added the seas.  Also scales properly.

--- my-app-typescript/src/index.tsx
+++ my-app-typescript/src/index.tsx
@@ -2,13 +2,13 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
-import Hello from './components/Hello';
+// import Hello from './components/Hello';
 import Map from './components/Map';
 
-ReactDOM.render(
+/*ReactDOM.render(
   <Hello/>,
   document.getElementById('root') as HTMLElement
-);
+); */
 ReactDOM.render(
   <Map/>,
   document.getElementById('map') as HTMLElement
