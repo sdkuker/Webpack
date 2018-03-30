fd197450277d8decf30258fd62278fe835f573cb
sdkuker <sdkuker@gmail.com>
2018-03-30 14:52:13 -0500

    Does it better now.

--- my-app-typescript/src/components/Fleet.tsx
+++ my-app-typescript/src/components/Fleet.tsx
@@ -6,12 +6,11 @@ class Fleet extends React.Component<ICircleProperties> {
 
     render() {
         return (
-            <svg className={this.props.className}>
-                <g transform="translate(162, 281)">
-                    <polygon points="-2,-3 10,-3 -2,-13" />
-                    <polygon points="-12,-1 -6,5 6,5 12,-1" />
-                </g>
-            </svg>
+            <g className={this.props.className} transform={'translate(' + this.props.x + ', ' + this.props.y + ')'}>
+                <polygon points="-2,-3 10,-3 -2,-13" />
+                <polygon points="-12,-1 -6,5 6,5 12,-1" />
+            </g>
+
         );
     }
 
