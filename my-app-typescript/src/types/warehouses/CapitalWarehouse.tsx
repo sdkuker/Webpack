c925849566e07c5253b4ce58519b0c4fada1bd49
sdkuker <sdkuker@gmail.com>
2018-04-10 21:11:47 -0500

    Added the beginning capitals.

--- my-app-typescript/src/types/warehouses/CapitalWarehouse.tsx
+++ my-app-typescript/src/types/warehouses/CapitalWarehouse.tsx
@@ -16,8 +16,34 @@ class CapitalWarehouse {
         const myMap = new Map<String, Capital>();
         const myLocations = LocationWarehouse.getLocations();
 
+        this.insertCapital(myMap, myLocations, 'Vienna', 'Austria');
+        this.insertCapital(myMap, myLocations, 'Budapest', 'Austria');
+        this.insertCapital(myMap, myLocations, 'Trieste', 'Austria');
+
         this.insertCapital(myMap, myLocations, 'London', 'England');
         this.insertCapital(myMap, myLocations, 'Edinburgh', 'England');
+        this.insertCapital(myMap, myLocations, 'Liverpool', 'England');
+
+        this.insertCapital(myMap, myLocations, 'Paris', 'France');
+        this.insertCapital(myMap, myLocations, 'Marseilles', 'France');
+        this.insertCapital(myMap, myLocations, 'Brest', 'France');
+
+        this.insertCapital(myMap, myLocations, 'Berlin', 'Germany');
+        this.insertCapital(myMap, myLocations, 'Munich', 'Germany');
+        this.insertCapital(myMap, myLocations, 'Kiel', 'Germany');
+
+        this.insertCapital(myMap, myLocations, 'Rome', 'Italy');
+        this.insertCapital(myMap, myLocations, 'Venice', 'Italy');
+        this.insertCapital(myMap, myLocations, 'Naples', 'Italy');
+
+        this.insertCapital(myMap, myLocations, 'Moscow', 'Russia');
+        this.insertCapital(myMap, myLocations, 'Sevastopol', 'Russia');
+        this.insertCapital(myMap, myLocations, 'StPetersburg', 'Russia');
+        this.insertCapital(myMap, myLocations, 'Warsaw', 'Russia');
+
+        this.insertCapital(myMap, myLocations, 'Ankara', 'Turkey');
+        this.insertCapital(myMap, myLocations, 'Constantinople', 'Turkey');
+        this.insertCapital(myMap, myLocations, 'Smyrna', 'Turkey');
         
         // myMap.set('London' + this.LocationTypes.PIECE,  new Location('162', '281'));
 
