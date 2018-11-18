8c8cfb2e52e2afeba5d97a34f1561e7e45278a5a
sdkuker <sdkuker@gmail.com>
2018-11-18 17:56:10 -0600

    got the nav working with keys in the url

--- my-app-typescript/src/components/gameManagement/GameManagementComponent.tsx
+++ my-app-typescript/src/components/gameManagement/GameManagementComponent.tsx
@@ -2,11 +2,12 @@ import * as React from 'react';
 import { observer } from 'mobx-react';
 import GameListComponent from './GameListComponent';
 import GameManagementButtonComponent from './GameManagementButtonComponent';
-import { Game } from '../../types/warehouses/Game';
+import { Redirect } from 'react-router-dom';
 import { IGameWarehouse } from '../../types/warehouses/IGameWarehouse';
 
 interface StateValues {
     selectedGameId: String | null;
+    redirectPath: String | null;
 }
 
 interface PropValues {
@@ -22,35 +23,65 @@ class GameManagementComponent extends React.Component<PropValues, StateValues> {
         this.openSelectedGame = this.openSelectedGame.bind(this);
         this.addGame = this.addGame.bind(this);
         this.deleteGame = this.deleteGame.bind(this);
+
+        this.state = { selectedGameId: null, redirectPath: null };
     }
 
     render() {
-        return (
-            <div>
-                <div className="jumbotron text-center">
-                    <h1>Stevieware Diplomacy</h1>
-                    <h2>Games</h2>
-                </div>
+
+        // tslint:disable-next-line
+        let theReturn: any = [];
+        if (this.state.redirectPath) {
+            if (this.state.redirectPath === 'openGame') {
+                const myPath = '/game/' + this.state.selectedGameId;
+                theReturn.push(<Redirect to={myPath} />);
+            } else {
+                if (this.state.redirectPath === 'newGame') {
+                    const myPath = '/newGame/';
+                    theReturn.push(<Redirect to={myPath} />);
+                } else {
+                    if (this.state.redirectPath === 'deleteGame') {
+                        const myPath = '/delete/' + this.state.selectedGameId;
+                        theReturn.push(<Redirect to={myPath} />);
+                    } else {
+                        theReturn.push(<Redirect to="/admin" />);
+                    }
+                }
+            }
+        } else {
+            theReturn.push(
+                (
+                    <div className="jumbotron text-center">
+                        <h1>Stevieware Diplomacy</h1>
+                        <h2>Games</h2>
+                    </div>));
+
+            theReturn.push(
                 <GameListComponent
                     gameWarehouse={this.props.gameWarehouse}
                     whenGameSelected={this.gameSelected}
-                />
+                />);
+
+            theReturn.push(
                 <GameManagementButtonComponent
                     whenOpenGameClicked={this.openSelectedGame}
                     whenAddGameClicked={this.addGame}
                     whenDeleteGameClicked={this.deleteGame}
-                />
+                />);
+        }
+        return (
+            <div>
+                {theReturn}
             </div>
         );
     }
 
     gameSelected(aGameId: String) {
         this.setState({ selectedGameId: aGameId });
-        console.log('gave saved: ' + aGameId);
     }
 
     openSelectedGame() {
-        console.log('open the game');
+        this.setState({ redirectPath: 'openGame' });
     }
 
     addGame() {
